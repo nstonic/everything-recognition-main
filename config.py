06ab15596738296f4e6c0625154cce6eb8f25255
nstonic <118763806+nstonic@users.noreply.github.com>
2023-02-02 23:05:00 +0400

    Make pathes independent of the os

--- config.py
+++ config.py
@@ -1,31 +1,33 @@
+from os.path import join
+
 CASCADES = {
     "Face front": {
-        "path": "haarcascades/faces/haarcascade_frontalface_default.xml",
+        "path": join("haarcascades", "faces", "haarcascade_frontalface_default.xml"),
         "color": (255, 0, 0),
         "draw": True
     },
     "Face profile": {
-        "path": "haarcascades/faces/haarcascade_profileface.xml",
+        "path": join("haarcascades", "faces", "haarcascade_profileface.xml"),
         "color": (255, 0, 0),
         "draw": True
     },
     "Smile": {
-        "path": "haarcascades/faces/haarcascade_smile.xml",
+        "path": join("haarcascades", "faces", "haarcascade_smile.xml"),
         "color": (0, 255, 0),
         "draw": False
     },
     "Eyes": {
-        "path": "haarcascades/faces/haarcascade_eye.xml",
+        "path": join("haarcascades", "faces", "haarcascade_eye.xml"),
         "color": (0, 255, 0),
         "draw": True
     },
     "Full body": {
-        "path": "haarcascades/faces/haarcascade_fullbody.xml",
+        "path": join("haarcascades", "faces", "haarcascade_fullbody.xml"),
         "color": (0, 255, 0),
         "draw": False
     },
     "Cat face": {
-        "path": "haarcascades/faces/haarcascade_frontalcatface_extended.xml",
+        "path": join("haarcascades", "faces", "haarcascade_frontalcatface_extended.xml"),
         "color": (0, 255, 0),
         "draw": False
     }
