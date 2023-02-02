d63b0b65e9f0345656b50796315fc3c9725e93cc
nstonic <118763806+nstonic@users.noreply.github.com>
2023-02-02 23:05:08 +0400

    Update Fixed run.py

--- Fixed run.py
+++ Fixed run.py
@@ -11,43 +11,61 @@ def show_frame(frame):
     cv2.imshow('Video', frame)
 
 
-def draw_sqare(frame, color):
-    cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+def draw_sqare(frame, color: tuple, rectangle: tuple):
+    x, y, w, h = rectangle
+    cv2.rectangle(
+        img=frame,
+        pt1=(x, y),
+        pt2=(x + w, y + h),
+        color=color,
+        thickness=2
+    )
 
 
-def get_cascades():
-    cascades = [
-        (cv2.CascadeClassifier(cascade['path']), cascade['color'])
-        for title, cascade in CASCADES.items()
-        if cascade['draw']
-    ]
-    return cascades
+def get_rectangle(frame, cascade) -> tuple:
+    return cascade.detectMultiScale(
+        frame,
+        scaleFactor=1.1,
+        minNeighbors=10,
+        minSize=(30, 30)
+    )
+
+
+def render_video(video, cascades):
+    while True:
+        if is_user_wants_quit():
+            break
+
+        if not video.isOpened():
+            print("Couldn't find your webcam... Sorry :c")
+
+        _, webcam_frame = video.read()
+        gray_frame = cv2.cvtColor(webcam_frame, cv2.COLOR_BGR2GRAY)
+
+        captures = (
+            (color, get_rectangle(frame=gray_frame, cascade=cascade))
+            for color, cascade in cascades
+        )
+
+        for color, capture in captures:
+            for rectangle in capture:
+                draw_sqare(webcam_frame, color, rectangle)
+
+        show_frame(webcam_frame)
 
 
 def main():
-    cascades = get_cascades()
-    video_capture = cv2.VideoCapture(0)
-    while True:
-        if not video_capture.isOpened():
-            print("Couldn't find your webcam... Sorry :c")
-        _, webcam_frame = video_capture.read()
-        gray_frame = cv2.cvtColor(webcam_frame, cv2.COLOR_BGR2GRAY)
-        for cascade, color in cascades:
-            captures = [cascade.detectMultiScale(
-                gray_frame,
-                scaleFactor=1.1,
-                minNeighbors=10,
-                minSize=(30, 30)
-            )]
-            for capture in captures:
-                for (x, y, w, h) in capture:
-                    draw_sqare(webcam_frame, color)
-        show_frame(webcam_frame)
-
-        if is_user_wants_quit():
-            break
-    video_capture.release()
-    cv2.destroyAllWindows()
+    cascades = (
+        (cascade['color'], cv2.CascadeClassifier(cascade['path']))
+        for _, cascade in CASCADES.items()
+        if cascade['draw']
+    )
+    video = cv2.VideoCapture(0)
+    try:
+        render_video(video, cascades)
+    finally:
+        video.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
